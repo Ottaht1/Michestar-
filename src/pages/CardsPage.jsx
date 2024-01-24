3331239c32c0b1a1c46d9bed0cdb686caec8824f
David Edu <daveeedu@gmail.com>
2024-01-24 08:11:02 +0100

    fix/added footer to all screens

--- src/pages/CardsPage.jsx
+++ src/pages/CardsPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const CardsPage = () => {
   return (
     <>
     <Navbar />
-
+    
+    <Footer />
     </>
   )
 }
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const ProductPage = () => {
   return (
     <>
     <Navbar />
 
+    <Footer />
     </>
   )
 }
